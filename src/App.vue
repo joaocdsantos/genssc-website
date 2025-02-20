d02eed6edaa48285c7de33fb0b67643371011a12
João Santos <joao_dias_santos@hotmail.com>
2025-02-20 23:15:49 +0000

    migration to vite

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="content">
+      <Logo></Logo>
+      <h2 @click="toggleClub">Gens SC</h2>
+      <Club v-if="showClub"></Club>
+
+      <h2 @click="toggleTeams">Equipas</h2>
+      <Teams v-if="showTeams"></Teams>
+
+      <h2 @click="toggleInfrastructures">Infraestrutura</h2>
+      <Infrastructures v-if="showInfrastructures"></Infrastructures>
+
+      <h2 @click="toggleLocation">Localização e Contato</h2>
+      <LocationContacts v-if="showLocation"></LocationContacts>
+
+      <CallAction></CallAction>
+
+      <auto-play-carousel></auto-play-carousel>
+
+      <Footer></Footer>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from "./components/Logo.vue";
+import Club from "./components/Club.vue";
+import Teams from "./components/Teams.vue";
+import Infrastructures from "./components/Infrastructures.vue";
+import CallAction from "./components/CallAction.vue";
+import LocationContacts from "./components/LocationContacts.vue";
+import Footer from "./components/Footer.vue";
+import AutoPlayCarousel from "./components/AutoPlayCarousel.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Logo,
+    Club,
+    Teams,
+    Infrastructures,
+    CallAction,
+    LocationContacts,
+    Footer,
+    AutoPlayCarousel,
+  },
+  data() {
+    return {
+      showClub: false,
+      showTeams: false,
+      showInfrastructures: false,
+      showLocation: false
+    }
+  },
+  methods: {
+    toggleClub() {
+      this.showClub = !this.showClub;
+    },
+    toggleTeams() {
+      this.showTeams = !this.showTeams;
+    },
+    toggleInfrastructures() {
+      this.showInfrastructures = !this.showInfrastructures;
+    },
+    toggleLocation() {
+      this.showLocation = !this.showLocation;
+    }
   }
 }
 </script>
@@ -22,5 +79,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  position: relative;
+  min-height: 100vh; /* Garante que o conteúdo ocupe pelo menos toda a altura da viewport */
 }
 </style>
