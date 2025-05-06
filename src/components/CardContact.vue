61898dbc43964aed731f25cff33b906ec3c50345
João Santos <joao_dias_santos@hotmail.com>
2025-05-06 21:08:16 +0100

    hrefs on mail and contact added lucile vue icons style definition

--- src/components/CardContact.vue
+++ src/components/CardContact.vue
@@ -1,13 +1,27 @@
 <template>
-  <section class="card">
+  <section class="card" id="contacts">
     <h1>Contacta-nos</h1>
-    <p>telefone</p>
-    <p>email</p>
-    <button class="btn">Entrar em Contacto</button>
+    <a href="mailto:geral@genssc.pt" class="contact-item">
+      <MailPlus />
+      <span> geral@genssc.pt</span>
+    </a>
+    <a href="tel:+351224541250" class="contact-item">
+      <PhoneForwarded />
+      <span> 224 541 250</span>
+    </a>
+
+    <!-- <a href="mailto:geral@genssc.pt"><p>geral@genssc.pt</p></a>
+    <a href="tel:+351224541250"><p>224 541 250</p></a>
+    <button class="btn">Envia mensagem</button> -->
   </section>
 </template>
-
+<script setup>
+import { PhoneForwarded, MailPlus } from 'lucide-vue-next';
+</script>
 <style scoped>
+section {
+  scroll-margin-top: 80px;
+}
 .card {
   min-height: 30vh;
   width: 100%;
@@ -18,19 +32,32 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-bottom: 50px;
 }
 .card h1 {
   font-size: 1.7rem;
   margin-bottom: 0.5rem;
 }
-.card img {
-  max-width: 250px;
-  opacity: 0.8;
+.contact-item {
+  display: inline-flex;
+  align-items: center;
+  gap: 1rem;
+  font-weight: 600;
+  color: var(--green);
+  text-decoration: none;
+  background-color: var(--white);
+  padding: 0.75rem 1.2rem;
+  border-radius: 8px;
+  margin-top: 0.8rem;
+  transition: background 0.2s ease;
 }
-.btn {
-  margin-top: 1rem;
-  padding: 0.75rem 1.5rem;
-  border: none;
-  border-radius: 10px;
+
+.contact-item:hover {
+  background-color: var(--green-light, #3a7d5e);
+}
+
+.contact-item svg {
+  width: 20px;
+  height: 20px;
 }
 </style>
