c6ab25b42a27b7cc3ce32e953b7fefe0f46dd988
João Santos <joao_dias_santos@hotmail.com>
2025-05-11 00:14:50 +0100

    location and cards adress implementation

---
+++ src/components/CardAddress.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="card" :style="cardStyle">
+    <img :src="imageSrc" alt="Foto" class="card-image" />
+    <div class="card-content">
+      <h2>{{ title }}</h2>
+      <p>{{ address }}</p>
+      <small>{{ zip }}</small>
+
+      <a :href="mapLink" target="_blank" rel="noopener noreferrer" class="btn-link"
+        ><MapPinHouse class="marker-icon" /> Google Maps
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Pointer, MapPinHouse } from 'lucide-vue-next';
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  title: String,
+  address: String,
+  zip: String,
+  mapsQuery: String,
+  color: String,
+  backgroundColor: String,
+  imageSrc: String,
+});
+
+const mapLink = computed(() => `https://www.google.com/maps?q=${encodeURIComponent(props.mapsQuery)}`);
+
+const cardStyle = computed(() => ({
+  color: props.color,
+  backgroundColor: props.backgroundColor,
+}));
+</script>
+
+<style scoped>
+.card {
+  width: 100%;
+  text-align: center;
+  padding: 2rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.card-content {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.6rem;
+}
+.card-image {
+  width: 300px;
+  height: auto;
+  object-fit: cover;
+  border-radius: 12px;
+  margin-bottom: 1.2rem;
+}
+.card h2 {
+  font-size: 1.3rem;
+  text-align: center;
+}
+
+.card a {
+  text-decoration: none;
+}
+
+.inline-icon {
+  vertical-align: middle;
+  display: inline-flex;
+  margin-left: 4px;
+  width: 1em;
+  height: 1em;
+}
+
+.marker-icon {
+  width: 1.2em;
+  height: 1.2em;
+  margin-right: 6px;
+  vertical-align: middle;
+}
+</style>
--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -1,10 +1,27 @@
 <template>
   <section class="location-section">
     <h1>Localização</h1>
+    <CardAddress
+      title="Parque Desportivo de Gens"
+      address="R. do Parque de Jogos, sn"
+      zip="4515-666 Gens - Foz do Sousa"
+      mapsQuery="41.119293707958306,-8.483172701985984"
+      color="var(--white)"
+      background-color="var(--green)"
+      :imageSrc="estadioImg"
+    />
+    <CardAddress
+      title="Sede Social"
+      address="R. Damião de Sousa 92"
+      zip="4515-123 Gens - Foz do Sousa"
+      mapsQuery="41.11488870881417,-8.485025038543432"
+      color="var(--green)"
+      backgroundColor="var(--white)"
+      :imageSrc="sedeImg"
+    />
 
-    <!-- INSERIR AS LOCALIZAÇõES-->
     <div class="slogan">
-      <span>Gens Sport Clube</span>
+      <span>Gens Sport Clube.</span>
       <span>O Gens que nos une.</span>
     </div>
 
@@ -18,25 +35,29 @@
   </section>
 </template>
 
+<script setup>
+import CardAddress from '../components/CardAddress.vue';
+
+import estadioImg from '../assets/gallery/estadio.jpg';
+import sedeImg from '../assets/gallery/gsc_test2.jpg';
+</script>
+
 <style scoped>
 .location-section {
   margin-top: 110px;
-  padding: 0 1.5rem;
-  max-width: 850px;
-  margin-left: auto;
-  margin-right: auto;
+  width: 100%;
+  color: var(--green);
 }
 
 h1 {
   text-align: center;
-  margin-bottom: 0.5rem;
+  margin-bottom: 3rem;
 }
-
 .back-link {
   display: block;
   text-align: center;
   margin-bottom: 2rem;
-  color: #3b82f6;
+  color: var(--link);
   font-weight: 500;
   text-decoration: none;
 }
@@ -70,6 +91,5 @@ h1 {
   text-align: center;
   font-weight: bold;
   font-size: 1.2rem;
-  color: #14532d;
 }
 </style>
